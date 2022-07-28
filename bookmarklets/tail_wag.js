514ea5e137694dfd5c2f421760027fed406914a7
agicitag <110203330+agicitag@users.noreply.github.com>
2022-07-29 01:53:34 +0200

    Number wags customizable

--- bookmarklets/tail_wag.js
+++ bookmarklets/tail_wag.js
@@ -1,15 +1,16 @@
 javascript: (() => {
 
-var mainTail = "PuppyTailStrap1";
-var secondaryTail = "WolfTailStrap3";
-var mainTailColor = "#431A12"; /* You can also set multiple colors like ["#431A12", "Default"] */
-var secondaryTailColor = "#310D0C";
-var delay = 1000; /* in ms */
+  var mainTail = "PuppyTailStrap1";
+  var secondaryTail = "WolfTailStrap3";
+  var mainTailColor = "#431A12"; /* You can also set multiple colors like ["#431A12", "Default"] */
+  var secondaryTailColor = "#310D0C";
+  var delay = 1000; /* in ms */
+  var numberWags = 3;
 
-/* Do the animation */
-for(var i = 0; i<3;i++){
-  setTimeout(function(){InventoryWear(Player, secondaryTail, "TailStraps", secondaryTailColor);},i*delay);
-  setTimeout(function(){InventoryWear(Player, mainTail, "TailStraps", mainTailColor );},i*delay+delay/2);
-}
+  /* Do the animation */
+  for(var i = 0; i<numberWags;i++){
+    setTimeout(function(){InventoryWear(Player, secondaryTail, "TailStraps", secondaryTailColor);},i*delay);
+    setTimeout(function(){InventoryWear(Player, mainTail, "TailStraps", mainTailColor );},i*delay+delay/2);
+  }
 
 })();
