87618f07eebd22fb6a095e1c3514356762f9cb61
agicitag <110203330+agicitag@users.noreply.github.com>
2022-08-18 21:14:18 +0200

    Add the possibility to display a message

--- bookmarklets/tail_wag.js
+++ bookmarklets/tail_wag.js
@@ -6,11 +6,29 @@ javascript: (() => {
   var secondaryTailColor = "#310D0C";
   var delay = 1000; /* in ms */
   var numberWags = 3;
+  var displayMessage = false; /* true or false */
+  var message = " wags her tail.";
 
   /* Do the animation */
   for(var i = 0; i<numberWags;i++){
     setTimeout(function(){InventoryWear(Player, secondaryTail, "TailStraps", secondaryTailColor);},i*delay);
     setTimeout(function(){InventoryWear(Player, mainTail, "TailStraps", mainTailColor );},i*delay+delay/2);
+  }
+  
+  /* Display the message */
+  if (displayMessage){
+    ServerSend("ChatRoomChat", {
+      Content: "Beep",
+      Type: "Action",
+      Target: null,
+      Dictionary: [
+        { Tag: "Beep", Text: "msg" },
+        { Tag: "Biep", Text: "msg" },
+        { Tag: "Sonner", Text: "msg" },
+        { Tag: "发送私聊", Text: "msg" },
+        { Tag: "msg", Text: Player.Nickname + message}
+      ]
+    });
   }
 
 })();
