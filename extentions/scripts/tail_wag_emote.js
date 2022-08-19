ac7a15b8289b2b921ab08a264f307517ea27fd8c
Your Name <you@example.com>
2022-08-19 17:35:34 +0530

    Added wait for ServerSocket

--- extentions/scripts/tail_wag_emote.js
+++ extentions/scripts/tail_wag_emote.js
@@ -1,4 +1,8 @@
-function TailWagEmote() {
+async function TailWagEmote() {
+
+    await waitFor(() => ServerIsConnected && ServerSocket);
+    var debug = 0;
+
     let tailVariations = ["FoxTailStrap2", "FoxTailStrap1"]; // change based on tail type
     let tailColor = ["Default", "#6E0232"]; // change color based on your own preference 
     let numberWags = 10;
@@ -13,17 +17,33 @@ function TailWagEmote() {
                 }, i * delay);
             }
         }
-    var GetEmote = function(data){
-      if(data.Type === "Emote" && data.Sender === Player.MemberNumber){
+    var GetEmote = function(data) {
+    if(data.Type === "Emote" && data.Sender === Player.MemberNumber){
         var message = data.Content;
         let pattern = /wags \w* tail/mi ; // matches {<any> wags <any single word or none> tail <any>}
         let result = pattern.test(message);
         if(result){
         TailWag(numberWags,tailColor);
-      }
+    }
     }
     };
     ServerSocket.on("ChatRoomMessage",GetEmote);
+    
+    async function waitFor(func, cancelFunc = () => false) {
+        while (!func()) {
+          if (cancelFunc()) {
+            if(debug === 1) console.log("waitFor returning false.");
+            return false;
+          }
+          if(debug === 1) console.log("waitFor sleep bit.");
+          await sleep(100);
+        }
+        if(debug === 1) console.log("waitFor returning true.");
+        return true;
+    }
+    function sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 }
 
 TailWagEmote();
