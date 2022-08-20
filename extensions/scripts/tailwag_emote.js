51924418ae1922a72a5e659e47336f36d12445a5
Haruhi <wdysylb@gmail.com>
2022-08-20 13:44:18 +0530

    Added command based changing for the tails

--- extensions/scripts/tailwag_emote.js
+++ extensions/scripts/tailwag_emote.js
@@ -2,31 +2,76 @@ async function TailWagEmote() {
 
     await waitFor(() => ServerIsConnected && ServerSocket);
 
-    let tailVariations = ["FoxTailStrap2", "FoxTailStrap1"]; // change based on tail type
-    let tailColor = ["Default", "#6E0232"]; // change color based on your own preference 
-    let numberWags = 10;
-    let delay = 500; // delay in ms
-        function TailWag(times, color)
+    var tailDefault = {
+        "strap1" : "FoxTailStrap1", // change based on tail type
+        "strap2" : "FoxTailStrap2",
+        "color1" : ["Default", "#6E0232"], // change color based on your own preference
+        "color2" : ["Default", "#6E0232"],
+        "count" : 10, // no. of tail wags
+        "delay" : 500, // delay in ms
+    };
+    
+    function TailWag()
+    {
+        let tailVariations = [tailDefault.strap2,tailDefault.strap1]; 
+        let tailColor = [tailDefault.color2,tailDefault.color1];  
+        let numberWags = tailDefault.count;
+        let delay = tailDefault.delay;
+        for(let i=0; i < numberWags; i++)
         {
-            for(let i=0; i < times; i++)
-            {
-                setTimeout(function() {
-                    InventoryWear(Player, tailVariations[i%tailVariations.length], "TailStraps", color);
-                    ChatRoomCharacterItemUpdate(Player, "TailStraps");
-                }, i * delay);
-            }
+            setTimeout(function() {
+                InventoryWear(Player, tailVariations[i%tailVariations.length], "TailStraps", tailColor[i%tailColor.length]);
+                ChatRoomCharacterItemUpdate(Player, "TailStraps");
+            }, i * delay);
         }
+    }
     var GetEmote = function(data) {
     if(data.Type === "Emote" && data.Sender === Player.MemberNumber){
         var message = data.Content;
         let pattern = /wags \w* tail/mi ; // matches {<any> wags <any single word or none> tail <any>}
         let result = pattern.test(message);
         if(result){
-        TailWag(numberWags,tailColor);
+        TailWag();
     }
     }
     };
     ServerSocket.on("ChatRoomMessage",GetEmote);
+    
+    function CommandTailChange(argsList)
+	{
+		let change = argsList[0];
+		let changeto = argsList.slice(1);
+        
+        //console.log("change = "+ change, "changeto = "+ changeto);
+
+        if (change === "current1") {
+            strap = InventoryGet(Player,"TailStraps");
+            tailDefault.strap1 = strap.Asset.Name;
+            tailDefault.color1 = strap.Color;
+        }
+        else if (change === "current2") {
+            strap = InventoryGet(Player,"TailStraps");
+            tailDefault.strap2 = strap.Asset.Name;
+            tailDefault.color2 = strap.Color;
+        }
+        else{
+            tailDefault[change]? tailDefault[change] = changeto : console.log("Invalid Input");
+        }
+        
+	}
+
+    CommandCombine([
+		{
+			Tag: 'tailchange',
+			AutoComplete: args => {
+				
+			},
+			Action: args => {
+				CommandTailChange(args.split(" "));
+			}
+		}
+	])
+
     
     async function waitFor(func, cancelFunc = () => false) {
         while (!func()) {
@@ -40,6 +85,8 @@ async function TailWagEmote() {
     function sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
+
+
 }
 
 TailWagEmote();
