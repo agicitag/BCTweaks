c87d474d43ca6638bd185d3cda470bf2f5047d9d
Julia #80525 <110203330+agicitag@users.noreply.github.com>
2022-08-25 17:27:45 +0200

    testing loading barSplitter

---
+++ extensions/barSplitterLoader.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Tail wag script loader
+// @namespace    https://www.bondageprojects.com/
+// @version      0.1
+// @description  Allows tail wagging using emotes
+// @author       agicitag
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.src = "https://agicitag.github.io/BondageClubTools/barSplitter.js";
+    document.head.appendChild(script);
+})();
