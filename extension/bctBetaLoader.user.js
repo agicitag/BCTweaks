3e1638185a5d988247447c604fa1db86ee11cd3b
Julia #80525 <110203330+agicitag@users.noreply.github.com>
2022-12-08 16:08:36 +0100

    Update bctBetaLoader.user.js

--- extension/bctBetaLoader.user.js
+++ extension/bctBetaLoader.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         BCT Beta Loader
+// @name         BCTweaks Beta Loader
 // @namespace    https://www.bondageprojects.com/
-// @version      0.4.1
+// @version      1
 // @description  BC extension adding several little useful tweaks to BC.
 // @author       agicitag
-// @downloadURL  https://github.com/agicitag/BondageClubTools/raw/main/extension/bctBetaLoader.user.js
+// @downloadURL  https://github.com/agicitag/BCTweaks/raw/main/extension/bctBetaLoader.user.js
 // @match https://bondageprojects.elementfx.com/*
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
@@ -17,6 +17,6 @@
 (function() {
     'use strict';
     var script = document.createElement("script");
-    script.src = "https://agicitag.github.io/BondageClubTools/beta/extension/bct.js";
+    script.src = "https://agicitag.github.io/BCTweaks/beta/extension/bct.js";
     document.head.appendChild(script);
 })();
